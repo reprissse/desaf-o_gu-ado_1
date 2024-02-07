962bd74e3bedf9bb73e72505b9a1a8c030c81d30
Raquel Jimenez <raquel.jimenez.ramirez@gmail.com>
2024-02-07 18:55:51 -0300

    :sparkles: se ingresa operación N3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-/*Realizar operaciones con dos números. Pedir al usuario que ingrese dos números 
-diferentes y mayores a cero. Para ambos números, calcular lo siguiente: (1.5 Puntos) */
+/*EJERCICIO NÚMERO 2
+Realizar operaciones con dos números. Pedir al usuario que ingrese dos números 
+diferentes y mayores a cero. Para ambos números, calcular lo siguiente: (1.5 Puntos)
 
 //se crean los prompt para el usuario//
 let number1 = +prompt('ingresa un numero mayor a 0')
@@ -16,4 +17,20 @@ document.write(`la resta es ${resta}`+'</br>')
 document.write(`la división es ${division}`+'</br>')
 document.write(`la multiplicación es ${multiplicacion}`+'</br>')
 document.write(`el módulo es ${modulo}`+'</br>')
+FINALIZA EJERCICIO NÚMERO 2*/
+
+/*EJERCICIO NÚMERO 3
+Recrear un programa que pida al usuario ingresar la temperatura en grados Celsius y que la transforme a grados Kelvin y Fahrenheit. (2 Puntos) 
+Se debe considerar la siguiente información respecto a la equivalencia de las escalas de temperatura:  
+● 0 Grados Celsius corresponden a 273,15 Kelvin. La fórmula para convertirlos es 0 °C + 273.15 = 273.15 K 
+● 0 Grados Celsius corresponden a 32 Fahrenheit. La fórmula para convertirlos es (0 °C × 9/5) + 32 = 32 °F*/
+
+let celsius = +prompt('ingresa los grados Celsius')
+let kelvin = (celsius + 273.15)
+let fahrenheit = (celsius * (9/5)) + 32
+document.write(`${celsius} grados Celcius, equivale a ${kelvin} grados Kelvin y a ${fahrenheit} grados Fahrenheit`)
+
+
+
+
 
