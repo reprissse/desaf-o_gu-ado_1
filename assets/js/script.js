2e8ab7f7a8be7b6a0b9da0339c9920eff5a91b40
Raquel Jimenez <raquel.jimenez.ramirez@gmail.com>
2024-02-07 20:01:11 -0300

    :sparkles: se ingresa operación N4

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,14 +23,37 @@ FINALIZA EJERCICIO NÚMERO 2*/
 Recrear un programa que pida al usuario ingresar la temperatura en grados Celsius y que la transforme a grados Kelvin y Fahrenheit. (2 Puntos) 
 Se debe considerar la siguiente información respecto a la equivalencia de las escalas de temperatura:  
 ● 0 Grados Celsius corresponden a 273,15 Kelvin. La fórmula para convertirlos es 0 °C + 273.15 = 273.15 K 
-● 0 Grados Celsius corresponden a 32 Fahrenheit. La fórmula para convertirlos es (0 °C × 9/5) + 32 = 32 °F*/
+● 0 Grados Celsius corresponden a 32 Fahrenheit. La fórmula para convertirlos es (0 °C × 9/5) + 32 = 32 °F
 
+//se define el prompt al usuario/
 let celsius = +prompt('ingresa los grados Celsius')
+//se definen las formulas/
 let kelvin = (celsius + 273.15)
 let fahrenheit = (celsius * (9/5)) + 32
+//se define el texto para el resultado/
 document.write(`${celsius} grados Celcius, equivale a ${kelvin} grados Kelvin y a ${fahrenheit} grados Fahrenheit`)
+FINALIZA EJERCICIO NÚMERO 3*/
 
+/*EJERCICIO NÚMERO 4
+Crear un programa que pida al usuario una cantidad de días y que muestre su 
+equivalente en Años, Semanas y Días. Por ejemplo, si el usuario ingresa 373, el 
+resultado debe ser 1 año, 1 semana y 1 día. (3 Puntos) 
+Se debe considerar lo siguiente:  
+● 1 año tiene 365 días 
+● 1 semana tiene 7 días 
+Se recomienda usar la función Math.floor para obtener la parte entera de un número 
+decimal.*/
 
-
+//se definen las constantes/
+const año = 365
+const semanas = 7
+//se genera el prompt al usuario/
+let dias = +prompt('ingresa la cantidad de días')
+//se definen las formulas/
+let añoRedondeado = Math.floor(dias/año)
+let semanaRedondeado = (añoRedondeado % dias)
+let diasrestantes = dias - (añoRedondeado * año) - (semanaRedondeado * semanas)
+//se genera el texto a visualizar/
+document.write(`${dias} son ${añoRedondeado} año, ${semanaRedondeado} semanas y ${diasrestantes} días`)
 
 
